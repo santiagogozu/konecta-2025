bde4877bd378673d080ada7bb324e096dfb30367
Santiago Gonzalez <santiagogo1993@gmail.com>
2025-02-01 17:15:44 -0500

    creacion de docker-composer y dockerfile para front y back cambio de libreria para ejecucion de forntend

--- Front/src/index.js
+++ Front/src/index.js
@@ -1,16 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App.js";
+import {AuthProvider} from "./context/AuthContext.jsx";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-      <App />
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
